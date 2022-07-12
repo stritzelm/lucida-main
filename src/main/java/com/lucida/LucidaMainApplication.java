c0baff55be7a59f6b26a9f27d95877cc39784fc0
Matthew Stritzel <matt.stritzel@flywire.com>
2022-07-12 15:51:12 -0500

    Initializing a Java Spring Project which we will use to build our own web app

---
+++ src/main/java/com/lucida/LucidaMainApplication.java
@@ -0,0 +1,15 @@
+package com.lucida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucidaMainApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(LucidaMainApplication.class, args);
+	}
+
+}
