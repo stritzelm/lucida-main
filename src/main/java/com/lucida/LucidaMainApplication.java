b10f28ff06980016192d01962bc4af9b7570a7c5
Matthew Stritzel <matt.stritzel@flywire.com>
2022-07-12 16:02:41 -0500

    Showing malik a code change

--- src/main/java/com/lucida/LucidaMainApplication.java
+++ src/main/java/com/lucida/LucidaMainApplication.java
@@ -10,6 +10,7 @@ public class LucidaMainApplication
 	public static void main(String[] args)
 	{
 		SpringApplication.run(LucidaMainApplication.class, args);
+		System.out.println("Code change");
 	}
 
 }
