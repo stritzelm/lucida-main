7b0b51c9a7c7e734bc6526603244da8c7fe7098a
Matt Stritzel <matthew.stritzel33@gmail.com>
2022-08-11 13:48:18 -0500

    Go Live MVP - deploy spring app using .jar file & aws EC2 (Java 11 + MySQL 8)

--- src/main/java/com/lucida/LucidaMainApplication.java
+++ src/main/java/com/lucida/LucidaMainApplication.java
@@ -2,8 +2,10 @@ package com.lucida;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.lucida.repository")
 public class LucidaMainApplication
 {
 
